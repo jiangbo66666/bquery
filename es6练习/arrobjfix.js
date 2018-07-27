641079acf52b124f5c18264fd63cae8f4eee577a
jiangbo <jiangbo1996@outlook.com>
2018-07-27 08:41:41 +0800

    早上练习下

--- es6练习/arrobjfix.js
+++ es6练习/arrobjfix.js
@@ -2,3 +2,9 @@ let arr1 = [32,345,13];
 let arr2 = [87,123,46];
 let arr3 = [...arr1,...arr2];
 console.log(arr3);
+//扩展运算符
+let arr = [2,1,4,1];
+let a = (...obj) => { //此处是rest,合并操作。
+  console.log(obj)
+}
+a(...arr);//此处是解构运算

01a4027ae1cf8887be558a01c2e0c00146d98a0e
jiangbo <jiangbo1996@outlook.com>
2018-08-04 08:52:45 +0800

    8.4morning2

--- 05node/06中间件传递数据2/server.js
+++ 05node/06中间件传递数据2/server.js
@@ -2,33 +2,49 @@ const express = require('express')
 
 const fs = require('fs')
 
-const querystring = require('querystring')
-
 const bodyparser = require('body-parser')
+
+const querystring = require('querystring')
 
 const server = express()
 
+server.use(writedate)
+
+// server.use(forminformetion)
+//替换为第三方插件
 server.use(bodyparser.urlencoded({extended:false}))
 
-server.use(writedata)
-
-server.post('/',(req,res)=>{
-  res.send(JSON.stringify(req.body))
+server.get('/index',(req,res)=>{
+  res.send('这是首页')
 })
 
-server.get('/about',(req,res)=>{
-  res.send('哈哈哈')
+server.post('/',(req,res)=>{
+  res.send(req.body)
 })
 
 server.listen(3000,()=>{
-  console.log('波神node启动')
+  console.log("波神启动")
 })
 
-function writedata(req,res,next){
-  const str = `请求时间 ${ new Date() } 请求方式 ${ req.method } 亲求地址 ${ req.url }\r\n`
-  fs.appendFile('./data.txt',str,(err)=>{
-    if(err) return console.log('失败')
-    console.log('成功')
+//生成日志的中间件
+function writedate(req,res,next){
+  const str = `${ new Date() } ${ req.method } ${ req.url }\r\n`
+  fs.appendFile('./date2.txt',str,(err)=>{
+    console.log('写入日志成功')
   })
   next()
 }
+
+//接受form表单中ajax的中间件
+// function forminformetion(req,res,next){
+//   let str2 = ''
+//   req.on('data',(chunk)=>{
+//     str2+=chunk
+//   })
+//   req.on('end',()=>{
+//     console.log(str2)
+//     req.body = querystring.parse(str2)
+//     console.log(req.body)
+//     next()
+//   })
+// }
